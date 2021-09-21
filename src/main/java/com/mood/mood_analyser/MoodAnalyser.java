db8bda7821985a20a03d6e194280799558a7307b
Anusha-Jagannath <anushajaganath6@gmail.com>
2021-09-21 16:54:25 +0530

    Added: refactored code using constructor

--- src/main/java/com/mood/mood_analyser/MoodAnalyser.java
+++ src/main/java/com/mood/mood_analyser/MoodAnalyser.java
@@ -1,10 +1,35 @@
 package com.mood.mood_analyser;
 
+/**
+ * MoodAnalyser class contains method analyseMood which takes string as input
+ * and checks whether string is happy or sad using simple if statements
+ */
 public class MoodAnalyser {
+
+	String message;
+
+	// default constructor
+	public MoodAnalyser() {
+
+	}
+
+	// parameterized constructor to initialize message field
+	public MoodAnalyser(String message) {
+		this.message = message;
+	}
 
 	public String analyseMood(String string) {
 
 		if (string.equals("This is a sad message"))
+			return "SAD";
+		else
+			return "HAPPY";
+
+	}
+
+	public String analyseMood() {
+
+		if (this.message.equals("This is a sad message"))
 			return "SAD";
 		else
 			return "HAPPY";
--- src/test/java/com/mood/mood_analyser/MoodAnalyserTest.java
+++ src/test/java/com/mood/mood_analyser/MoodAnalyserTest.java
@@ -8,9 +8,31 @@ import org.junit.Test;
 public class MoodAnalyserTest {
 
 	@Test
-	public void moodAnalysis() {
+	public void whenGivenInput_AsSad_ShouldReturnSad() {
 		MoodAnalyser moodAnalyser = new MoodAnalyser();
 		String mood = moodAnalyser.analyseMood("This is a sad message");
 		assertThat(" ", mood, CoreMatchers.is("SAD"));
 	}
+
+	@Test
+	public void whenGivenInput_AsHappy_ShouldReturnHappy() {
+		MoodAnalyser moodAnalyser = new MoodAnalyser();
+		String mood = moodAnalyser.analyseMood("This is a happy message");
+		assertThat(" ", mood, CoreMatchers.is("HAPPY"));
+	}
+	
+	@Test
+	public void whenGivenInput_AsSad_UsingConstructor_ShouldReturnSad() {
+		MoodAnalyser moodAnalyser = new MoodAnalyser("This is a sad message");
+		String mood = moodAnalyser.analyseMood();
+		assertThat(" ", mood, CoreMatchers.is("SAD"));
+	}
+
+	@Test
+	public void whenGivenInput_AsHappy_UsingConstructor_ShouldReturnHappy() {
+		MoodAnalyser moodAnalyser = new MoodAnalyser("This is a happy message");
+		String mood = moodAnalyser.analyseMood();
+		assertThat(" ", mood, CoreMatchers.is("HAPPY"));
+	}
+
 }
